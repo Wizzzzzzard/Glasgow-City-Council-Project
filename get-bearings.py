7335203bb26898c659c4c4d70e77623e69883bf3
Wizzzzzzard <56865341+Wizzzzzzard@users.noreply.github.com>
2021-06-24 01:27:59 +0100

    Add files via upload

---
+++ get-bearings.py
@@ -0,0 +1,11 @@
+import math
+import numpy as np
+
+def get_bearing(lat1, long1, lat2, long2):
+    dLon = (long2 - long1)
+    x = math.cos(math.radians(lat2)) * math.sin(math.radians(dLon))
+    y = math.cos(math.radians(lat1)) * math.sin(math.radians(lat2)) - math.sin(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.cos(math.radians(dLon))
+    brng = np.arctan2(x,y)
+    brng = np.degrees(brng)
+
+    return brng
